18601fec2303ada7d2d88ceecc6a307592c5e757
inamali-palasaniya <inamali@intiqa.io>
2025-12-12 16:59:55 +0530

    fix(backend): Satisfy PrismaClient constructor type in script

--- backend/src/scripts/update_pass.ts
+++ backend/src/scripts/update_pass.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '../generated/client/client.js';
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({} as any);
 (async () => {
   await prisma.user.update({ where: { email: 'admin@sports.com' }, data: { password: 'b.i.8/z.d.d.d.d.d.e.d.d.d.d.d.d.d.d.d.d.d.d.d.d' } });
   console.log('Admin password updated');
