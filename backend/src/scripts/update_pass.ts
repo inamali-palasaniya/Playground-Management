7cc7b00d3fdb9cb3fa20d3b19b633eae5ba60951
inamali-palasaniya <inamali@intiqa.io>
2025-12-12 16:59:44 +0530

    fix(backend): Correct PrismaClient import path in scripts

--- backend/src/scripts/update_pass.ts
+++ backend/src/scripts/update_pass.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/client/index.js';
+import { PrismaClient } from '../generated/client/client.js';
 const prisma = new PrismaClient();
 (async () => {
   await prisma.user.update({ where: { email: 'admin@sports.com' }, data: { password: 'b.i.8/z.d.d.d.d.d.e.d.d.d.d.d.d.d.d.d.d.d.d.d.d' } });
