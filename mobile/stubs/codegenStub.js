2eb4c33e26d1af9950017201d3860b9aedc134df
aincoders <inamali@garagebox.io>
2026-04-23 19:55:40 +0530

    fix(web): return valid react component from native stubs

--- mobile/stubs/codegenStub.js
+++ mobile/stubs/codegenStub.js
@@ -1,2 +1,15 @@
-module.exports = () => ({});
-module.exports.default = () => ({});
+const React = require('react');
+
+// A dummy React component that acts as a placeholder for native views on web
+const DummyView = React.forwardRef((props, ref) => {
+  return React.createElement('div', { ...props, ref }, props.children);
+});
+
+// A dummy function for native commands
+const dummyCommands = () => ({});
+
+// Export both as default and named to cover different import patterns
+module.exports = () => DummyView;
+module.exports.default = () => DummyView;
+module.exports.codegenNativeComponent = () => DummyView;
+module.exports.codegenNativeCommands = () => dummyCommands;
